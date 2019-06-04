8f2ed82244bdb798c3000133b54e199b1e8724b8
Tuan Dao <dmtuan@tma.com.vn>
2019-06-04 10:55:36 +0700

    update init

--- application/config/config.php
+++ application/config/config.php
@@ -25,7 +25,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 $base  = "http://".$_SERVER['HTTP_HOST'];
 $base .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
+
 $config['base_url'] = $base;
+
+$config['base_url'] = 'http://localhost:1111/ship/';
 
 /*
 |--------------------------------------------------------------------------
